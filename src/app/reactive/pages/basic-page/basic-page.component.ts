6b68e292cbd688ecbac0b1f23f4bb95237c89b4a
florius88 <florsantosbueno@gmail.com>
2023-04-24 18:28:11 +0200

    Re-establecer y establecer el valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+
+const rtx = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   /* public myForm: FormGroup = new FormGroup({
     // new FormControl(
     //  'valor por defecto', 
-    //  [validaciones síncronas], 
+    //  una única validación ó [validaciones síncronas], 
     //  [validaciones asíncronas]
     //  )
     name: new FormControl('', [], []),
@@ -21,16 +27,33 @@ export class BasicPageComponent {
   }); */
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    // Requerido y con una longitud mayor de 3 caracteres
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    // Requerido y mínimo valor 0
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   })
 
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset(rtx);
+  }
 
   onSave() {
+
+    if (this.myForm.invalid) return;
+
     console.log(this.myForm.value)
+
+    // REESTABLECER EL VALOR DEL FORMULARIO
+    /* El "problema" de trabajar con setValue es que necesita un input o un elemento
+    en particular del formulario
+    this.myForm.setValue()
+    */
+    this.myForm.reset({ price: 0, inStorage: 0 });
+
   }
 
 }
