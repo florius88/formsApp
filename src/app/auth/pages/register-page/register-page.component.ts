3f73c468d919e509fa420a4f75a4efa6fe3e292c
florius88 <florsantosbueno@gmail.com>
2023-04-25 17:00:33 +0200

    Validaciones de email y patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import { cantBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 
 @Component({
@@ -10,16 +10,16 @@ import { cantBeStrider } from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) { }
 
-  isValidField(field: string){
+  isValidField(field: string) {
     // TODO obtener validadión desde un servicio
   }
 
